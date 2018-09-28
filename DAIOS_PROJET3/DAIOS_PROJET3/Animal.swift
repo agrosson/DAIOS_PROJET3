06703df83973f00bf0e72e0d610f3038d5955799
agrosson <agrosson@gmail.com>
2018-09-28 14:28:23 +0200

    Ajout du fichier Animal.swift 1. Create Animal class

---
+++ DAIOS_PROJET3/DAIOS_PROJET3/Animal.swift
@@ -0,0 +1,17 @@
+//
+//  Animal.swift
+//  DAIOS_PROJET3
+//
+//  Created by ALEXANDRE GROSSON on 28/09/2018.
+//  Copyright © 2018 GROSSON. All rights reserved.
+//
+
+import Foundation
+
+class Animal {
+    var animalName: String
+    
+    init(animalName: String) {
+        self.animalName = animalName
+    }
+}
