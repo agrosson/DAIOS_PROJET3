e05e035c6abfc3a104739ca7b5fd091e56f1d008
agrosson <agrosson@gmail.com>
2018-09-28 15:34:30 +0200

    Misspelling variable and add test code area in main.swift (object creation and use of functions)

--- DAIOS_PROJET3/DAIOS_PROJET3/Fighter.swift
+++ DAIOS_PROJET3/DAIOS_PROJET3/Fighter.swift
@@ -11,7 +11,7 @@ import Foundation
 class Fighter {
     var fighterName: String
     var fighterLife = 0
-    var fighteraAnimal = [Animal]()
+    var fighterAnimal = [Animal]()
     
     init(fighterName: String){
         self.fighterName = fighterName
@@ -20,6 +20,6 @@ class Fighter {
     func description() -> String {
         return "My name is \(fighterName)"
                 + "\n  1. My life level: \(fighterLife)"
-                + "\n  2. I have got \(fighteraAnimal.count) " + (fighteraAnimal.count > 1 ? "animals" : "animal")
+                + "\n  2. I have got \(fighterAnimal.count) " + (fighterAnimal.count > 1 ? "animals" : "animal")
     }
 }
--- DAIOS_PROJET3/DAIOS_PROJET3/main.swift
+++ DAIOS_PROJET3/DAIOS_PROJET3/main.swift
@@ -7,4 +7,22 @@
 //
 import Foundation
 
+
+//Test code
+
+
 print("Test")
+
+var monEqui = Team(name: "LoulouTeam")
+print(monEqui.presentation())
+
+var fight1 = Fighter(fighterName: "Loulou")
+//print(fight1.description())
+monEqui.teamMembers.append(fight1)
+print(monEqui.presentation())
+
+var chien = Animal(animalName: "Doggy")
+fight1.fighterAnimal.append(chien)
+print(monEqui.presentation())
+
+//Test code
